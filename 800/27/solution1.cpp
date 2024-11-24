232cf0bb244ddaba75dff7c6e1fa35d2e9a8a80d
Aniket Deore <aniketrdeore@gmail.com>
2024-11-24 18:29:00 +0530

    27

---
+++ 800/27/solution1.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+
+int main() {
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n, b, oneCount = 0, twoCount = 0;
+        cin >> n;
+        vector<ll> indices;
+        
+        for (ll i = 0; i < n; i++) {
+            cin >> b;
+            if (b == 1) {
+                oneCount++;
+            } else {
+                twoCount++;
+                indices.push_back(i + 1); 
+            }
+        }
+
+        if (twoCount % 2 != 0) {
+            cout << -1 << endl; 
+        } else {
+            if (twoCount == 0) {
+                cout << 1 << endl; 
+            } else {
+                ll middleIndex = indices.size() / 2;
+                cout << indices[middleIndex - 1] << endl;
+            }
+        }
+    }
+
+    return 0;
+}
