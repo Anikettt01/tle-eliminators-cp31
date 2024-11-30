df83dc28300f398ff284bc6b72362d2d54b04804
Aniket Deore <aniketrdeore@gmail.com>
2024-12-01 01:05:34 +0530

    Solved by Aniket

--- 900/12/solution1.cpp
+++ 900/12/solution1.cpp
@@ -11,19 +11,15 @@ void helper(){
 	for(int i = 0; i < n; ++i){
 		cin >> A[i];
 	}
-
 	for(int i = 0; i < n; ++i){
 		ans = max(ans, A[(i - 1 + n) % n] - A[i]);
 	}
-
 	for(int i = 1; i < n; ++i){
 		ans = max(ans, A[i] - A[0]);
 	}
-
 	for(int i = 0; i < n - 1; ++i){
 		ans = max(ans, A[n - 1] - A[i]);
 	}
-
 	cout << ans << '\n';
 }
 
