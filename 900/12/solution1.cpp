add992ddc79b2eb5174bdecb801f294a3e085958
Aniket Deore <aniketrdeore@gmail.com>
2024-12-21 22:37:24 +0530

    updated code

--- 900/12/solution1.cpp
+++ 900/12/solution1.cpp
@@ -5,22 +5,22 @@ void helper(){
 	int n;
 	cin>>n;
 
-	int A[n];
-	int ans = -1000000007;
+	int arr[n];
+	int ans = INT_MIN;
 
 	for(int i = 0; i < n; ++i){
-		cin >> A[i];
+		cin >> arr[i];
 	}
 	for(int i = 0; i < n; ++i){
-		ans = max(ans, A[(i - 1 + n) % n] - A[i]);
+		ans = max(ans, arr[(i - 1 + n) % n] - arr[i]);
 	}
 	for(int i = 1; i < n; ++i){
-		ans = max(ans, A[i] - A[0]);
+		ans = max(ans, arr[i] - arr[0]);
 	}
 	for(int i = 0; i < n - 1; ++i){
-		ans = max(ans, A[n - 1] - A[i]);
+		ans = max(ans, arr[n - 1] - arr[i]);
 	}
-	cout << ans << '\n';
+	cout << ans << endl;
 }
 
 int main(){
