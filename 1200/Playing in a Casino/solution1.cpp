f80aa8d9fcef3a4d32d16c0150238fe5ab67c43c
Aniket Deore <aniketrdeore@gmail.com>
2025-01-21 10:37:23 +0530

    Solved by Aniket

--- 1200/Playing in a Casino/solution1.cpp
+++ 1200/Playing in a Casino/solution1.cpp
@@ -1,44 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 typedef long long ll;
-typedef vector<int> vi;
-typedef vector<ll> vll;
 
 int main() {
     int t;
     cin >> t;
     while (t--) {
-        ll n,m;
-        cin>>n>>m;
-        // vll a(n),b(n),c(n);
-        // for(int i=0;i<n;i++){
-        //     cin>>a[i];
-        // }
-        // for(int i=0;i<n;i++){
-        //     cin>>b[i];
-        // }
-        // for(int i=0;i<n;i++){
-        //     cin>>c[i];
-        // }
-        vector<vector<ll>> arr(n,vector<ll>(m));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                cin>>arr[i][j];
+        ll n, m;
+        cin >> n >> m;
+        vector<vector<ll>> arr(n, vector<ll>(m));
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                cin >> arr[i][j];
             }
         }
-        ll ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                ll sum = 0;
-                for(int k=0;k<m;k++){
-                    sum += abs(arr[i][k]-arr[j][k]);
-                }
-                ans+=sum;
+        
+        ll ans = 0;
+        for(int j = 0; j < m; j++) {
+            vector<ll> column;
+            for(int i = 0; i < n; i++) {
+                column.push_back(arr[i][j]);
+            }
+            sort(column.begin(), column.end());
+            ll leftSum = 0;
+            ll totalSum = 0;
+            for(ll val : column) totalSum += val;
+            
+            for(int i = 0; i < n; i++) {
+                ll currVal = column[i];
+                ll rightSum = totalSum - leftSum - currVal;
+                ll contribution = (i * currVal - leftSum) + (rightSum - (n-1-i) * currVal);
+                ans += contribution;
+                leftSum += currVal;
             }
         }
-        cout<<ans<<endl;
+        ans /= 2;
+        cout << ans << endl;
     }
-    
     return 0;
 }
