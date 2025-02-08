d4db9393bfc33207fd02cb3d0a1bb23928dbf9ef
Aniket Deore <aniketrdeore@gmail.com>
2025-02-08 12:15:41 +0530

    Solved by Aniket

---
+++ 1000/23. Different Divisors/solution1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int d;
+        cin >> d;
+    }
+    
+    return 0;
+}
